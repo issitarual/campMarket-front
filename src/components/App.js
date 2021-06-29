8776c130bfa5e7819edc71e061ddb39f7126a6c7
Laura Tissi <laura.tracierra@gmail.com>
2021-06-29 08:46:34 -0300

    criando as rotas

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+
+
+
+import '../css/reset.css';
+
+export default function App(){
+    return(
+
+    );
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import App from './components/app';
+
+ReactDOM.render(<App />, document.querySelector(".root"));
